0a00f966f9563aeb3db2010d21cd050a46be7093
Matt Davis <davismatt503@gmail.com>
2018-05-31 10:43:14 -0700

    Update ImageChooser.php

--- Block/Adminhtml/Widget/Type/ImageChooser.php
+++ Block/Adminhtml/Widget/Type/ImageChooser.php
@@ -43,6 +43,7 @@ class ImageChooser extends Template
         $onInsertUrl = $this->getUrl('dmatthew_widgettypes/wysiwyg_images/onInsert');
         $sourceUrl = $this->getUrl('cms/wysiwyg_images/index',
             ['target_element_id' => $element->getId(), 'on_insert_url' => $onInsertUrl]);
+        
         /** @var \Magento\Backend\Block\Widget\Button $chooser */
         $chooser = $this->getLayout()->createBlock('Magento\Backend\Block\Widget\Button')
             ->setType('button')
@@ -50,6 +51,7 @@ class ImageChooser extends Template
             ->setLabel($config['button']['open'])
             ->setOnClick('MediabrowserUtility.openDialog(\'' . $sourceUrl . '\', 0, 0, "Insert File...", {})')
             ->setDisabled($element->getReadonly());
+        
         /** @var \Magento\Framework\Data\Form\Element\Text $input */
         $input = $this->elementFactory->create("text", ['data' => $element->getData()]);
         $input->setId($element->getId());
@@ -58,9 +60,11 @@ class ImageChooser extends Template
         if ($element->getRequired()) {
             $input->addClass('required-entry');
         }
+        
         $element->setData('after_element_html', $input->getElementHtml()
             . $chooser->toHtml() . "<script>require(['mage/adminhtml/browser']);</script>");
         $element->setValue('');  // Hides the additional label that gets added.
+        
         return $element;
     }
 }
